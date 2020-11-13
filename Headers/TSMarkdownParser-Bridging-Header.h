44f28fb0af213c82006fcdc51b5f1749613a3069
Eduardo Miranda <eduardo.miranda@massive.co>
2020-11-13 10:31:38 +0100

    spm part 5

---
+++ Headers/TSMarkdownParser-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  TSMarkdownParser-Bridging-Header.h
+//  TSMarkdownParser
+//
+//  Created by Eduardo Miranda on 13/11/2020.
+//  Copyright © 2020 Computertalk Sweden. All rights reserved.
+//
+
+#import "TSMarkdownParser.h"
